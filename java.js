7e83a5253585f8a9237d58bd7527b671ca0b8cb2
iiakash03 <iiakash59@gmail.com>
2023-01-17 18:25:16 +0530

    listchanged to bold

--- java.js
+++ java.js
@@ -3,3 +3,8 @@ document.title="My Name";
 let header=document.getElementById("header-title");
 console.log(header);
 header.style.border='solid 3px #000';
+let list=document.getElementsByClassName('list-group-item');
+list[2].style.backgroundColor='green';
+for(let i=0;i<list.length;i++){
+    list[i].style.fontWeight='Bold';
+}
